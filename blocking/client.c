9366318d5ed8191542fc45fd4804620a169efb76
Sneha Inguva <snehainguva@Snehas-MacBook-Pro-2.local>
2021-04-18 13:07:20 -0400

    client: sending data only once

--- blocking/client.c
+++ blocking/client.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
    int sock;
    struct sockaddr_in server;
    char server_reply[2000];
-   
+
    char message[1000] = "Hi";
 
    sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,22 +26,23 @@ int main(int argc, char *argv[]) {
 
    if (connect (sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
       perror("connect failed. error\n");
-      return 1;      
+      return 1;
    }
    printf("connected\n");
 
-   while(1) {
-      printf("sendng message\n");
-      if (send(sock, message, strlen(message), 0) < 0) {
-         printf("send failed\n");
-	 return 1;
-      }
-      printf("receiving message\n");
-      if (recv(sock, server_reply, 2000, 0) < 0) {
-         printf("recv failed\n");
-	 break;
-      }
-
-      printf("Server reply: %s\n", server_reply);
+   printf("sendng message\n");
+   if (send(sock, message, strlen(message), 0) < 0) {
+      printf("send failed\n");
+      return 1;
    }
+   
+   printf("receiving message\n");
+   if (recv(sock, server_reply, 2000, 0) < 0) {
+      printf("recv failed\n");
+      return 1;
+   }
+
+   printf("Server reply: %s\n", server_reply);
+   close(sock);
+   return 0;
 }
