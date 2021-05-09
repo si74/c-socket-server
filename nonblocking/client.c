9850c9f5aafdf7f1062d7b3324f7e0f13e22f2a2
ysabel <ysabel.c.tan@gmail.com>
2021-05-09 08:14:29 -0700

    added todo's

--- nonblocking/client.c
+++ nonblocking/client.c
@@ -4,22 +4,52 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#include <fcntl.h>
-#include <sys/time.h>
-#include <sys/types.h>
-
 const int PORT = 8080;
 
 int main(int argc, char *argv[]) {
-   int sockfd;
+   int sock;
    struct sockaddr_in server;
    char server_reply[2000];
 
-   sockfd = socket(PF_INET, SOCK_STREAM, 0);
-   fcntl(sockfd, F_SETFL, O_NONBLOCK);
+   char message[1000] = "Hi";
 
-   while 
-   select()
-    
+   sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+   if (sock == -1) {
+      printf("couldn't create socket\n");
+      return 1;
+   }
+   printf("socket created\n");
 
+   server.sin_addr.s_addr = inet_addr("127.0.0.1");
+   server.sin_family = AF_INET;
+   server.sin_port = htons(PORT);
+
+   if (connect (sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+      perror("connect failed. error\n");
+      return 1;
+   }
+   printf("connected\n");
+
+   printf("sendng message\n");
+   if (send(sock, message, strlen(message), 0) < 0) {
+      printf("send failed\n");
+      return 1;
+   }
+   
+   printf("receiving message\n");
+   if (recv(sock, server_reply, 2000, 0) < 0) {
+      printf("recv failed\n");
+      return 1;
+   }
+
+   printf("Server reply: %s\n", server_reply);
+   close(sock);
+   return 0;
 }
+
+// connect is blocking
+// set socket to non blocking 
+// add fd to master
+// connect
+// blocking select in for loop
+// when fd is ready, check sockopt for errors
