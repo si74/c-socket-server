a65f5371b0baf3291cd2a7a7e4acf8e969f73441
Sneha Inguva <snehainguva@Snehas-MacBook-Pro-2.local>
2021-07-25 18:57:05 -0400

    initial nonblocking code attempt

--- nonblocking/client.c
+++ nonblocking/client.c
@@ -3,39 +3,65 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 const int PORT = 8080;
 
 int main(int argc, char *argv[]) {
+   extern int errno;
+   int errnum;
    int sock;
+   int status;
    struct sockaddr_in server;
    char server_reply[2000];
 
    char message[1000] = "Hi";
 
-   sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+// TODO(sneha): create parent fd set
+
+   // For some reason SOCKSTREAM | O_NONBLOCK doesn't work
+   // despite what the man page says (https://man7.org/linux/man-pages/man2/socket.2.html).
+   // Hence, using fcntl() instead.
+   sock = socket(AF_INET, SOCK_STREAM, 0);
    if (sock == -1) {
-      printf("couldn't create socket\n");
+     perror("socket error");
+     return 1;
+   }
+   status = fcntl(sock, F_SETFL, O_NONBLOCK);
+   if (status < 0) {
+      perror("error setting socket to nonblocking");
       return 1;
    }
-   printf("socket created\n");
+   printf("non-blocking socket created\n");
 
    server.sin_addr.s_addr = inet_addr("127.0.0.1");
    server.sin_family = AF_INET;
    server.sin_port = htons(PORT);
 
    if (connect (sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
-      perror("connect failed. error\n");
+      errnum = errno;
+      // TODO(sneha): check type of error - i.e. in progress and continue if that is the case
+      if (errno == EINPROGRESS) {
+        printf("operation still in progress. continuing...");
+      }
+      perror("connect failed\n");
       return 1;
    }
    printf("connected\n");
+
+   // TODO(sneha): Add to fd set
+
+   // TODO(sneha: Do select in loop to see if socket is connected/ready
+   // when fd ready, check sockopt for errors
+   // send message
 
    printf("sendng message\n");
    if (send(sock, message, strlen(message), 0) < 0) {
       printf("send failed\n");
       return 1;
    }
-   
+
    printf("receiving message\n");
    if (recv(sock, server_reply, 2000, 0) < 0) {
       printf("recv failed\n");
@@ -48,7 +74,7 @@ int main(int argc, char *argv[]) {
 }
 
 // connect is blocking
-// set socket to non blocking 
+// set socket to non blocking
 // add fd to master
 // connect
 // blocking select in for loop
